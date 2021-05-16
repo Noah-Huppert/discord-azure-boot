7cb447440674c669ab7afeddc3bc1c6bad194c4e
Noah Huppert <contact@noahh.io>
2021-05-16 01:10:33 -0400

    added virtual machines to config, made prog verify vm cfg valid

--- config.ex.js
+++ config.ex.js
@@ -1,4 +1,7 @@
 module.exports = {
+    /**
+	* Azure client information.
+	*/
     azure: {
 	   /**
 	    * Azure subscription ID in which virtual machines are running.
@@ -20,4 +23,29 @@ module.exports = {
 	    */
 	   accessToken: "",
     },
+
+    /**
+	* Define the Azure virtual machines which the bot is allowed to manage. Array of objects which specify vm details.
+	*/
+    vms: [
+	   /**
+	    * Virtual machine details.
+	    */
+	   {
+		  /**
+		   * The name of the Azure resource group in which this virtual machine exists.
+		   */
+		  resourceGroup: "",
+		  
+		  /**
+		   * The name of the virtual machine in Azure.
+		   */
+		  azureName: "",
+
+		  /**
+		   * The name which users will see and use to refer to your virtual machine.
+		   */
+		  friendlyName: "",
+	   }
+    ],
 };
--- index.js
+++ index.js
@@ -11,11 +11,22 @@ class Bot {
 
     /**
 	* Performs initialization of Bot. Performs async operations which could not be performed in the constructor.
+	* @throws {Error} If the bot fails to initialize.
 	*/
     async init() {
+	   // Authenticate with the Azure API
 	   this.azureCreds = (await msRestNodeAuth.loginWithServicePrincipalSecretWithAuthResponse(this.cfg.azure.applicationID, this.cfg.azure.accessToken, this.cfg.azure.directoryID)).credentials;
 
 	   this.azureCompute = new ComputeManagementClient(this.azureCreds, this.cfg.azure.subscriptionID);
+
+	   // Ensure all the virtual machines the user specified actually exist
+	   try {
+		  await Promise.all(this.cfg.vms.map(async (vm) => {
+			 await this.azureCompute.virtualMachines.get(vm.resourceGroup, vm.azureName);
+		  }));
+	   } catch (e) {
+		  throw new Error(`Failed to find all virtual machines specified in the configuration: ${e}`);
+	   }
     }
 }
 
